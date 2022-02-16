c07b8900726e47b52865c3b950ea4b80a3bbbfbf
Wesley455 <wesley@drterapia.com>
2022-02-15 22:53:23 -0300

    adicionado o swagger para documentação

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,19 +1,24 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 import "express-async-errors";
 
 import "./bootstrap";
 import "@shared/container";
-import createConnection from "@shared/infra/typeorm";
 
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
+
+import swaggerFile from "../../../swagger.json";
 
 createConnection();
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
